bd10f27134a62323e32e17721149e64a2eccdcd4
Duncan Cross <duncan.cross@gmail.com>
2014-06-27 00:38:52 +0100

    Expand IFF support to include ILBM

--- lua/denzquix/packland/formats/iff.lua
+++ lua/denzquix/packland/formats/iff.lua
@@ -1,4 +1,6 @@
 
+local ffi = require 'ffi'
+local bit = require 'bit'
 local R = require 'denzquix.packland.reader'
 
 local format = {}
@@ -24,6 +26,24 @@ function format.dbinit(db)
 			volume INTEGER,
 
 			FOREIGN KEY (sample_dbid) REFERENCES audio_sample(dbid)
+		);
+
+		CREATE TABLE IF NOT EXISTS bitmap (
+			dbid INTEGER PRIMARY KEY,
+			pixel_format TEXT,
+			pixel_data BLOB,
+			palette BLOB,
+			width INTEGER,
+			height INTEGER
+		);
+
+		CREATE TABLE IF NOT EXISTS iff_ilbm (
+			dbid INTEGER PRIMARY KEY,
+			bitmap_dbid INTEGER,
+			x INTEGER,
+			y INTEGER,
+
+			FOREIGN KEY (bitmap_dbid) REFERENCES bitmap(dbid)
 		);
 
 	]])
@@ -97,6 +117,40 @@ function format.todb(intype, inpath, db, context)
 		assert( assert( exec_add_8svx:step() ) == 'done' )
 		assert( exec_add_8svx:finalize() )
 
+	elseif iff.type == 'ILBM' then
+
+		local exec_add_bitmap = assert(db:prepare [[
+
+			INSERT INTO bitmap (pixel_format, pixel_data, palette, width, height)
+			VALUES (:pixel_format, :pixel_data, :palette, :width, :height)
+
+		]])
+
+		assert( exec_add_bitmap:bind_text(':pixel_format', 'p8') )
+		assert( exec_add_bitmap:bind_blob(':pixel_data', iff.pixel_data) )
+		assert( exec_add_bitmap:bind_blob(':palette', iff.palette) )
+		assert( exec_add_bitmap:bind_int(':width', iff.width) )
+		assert( exec_add_bitmap:bind_int(':height', iff.height) )
+
+		assert( assert( exec_add_bitmap:step() ) == 'done' )
+		assert( exec_add_bitmap:finalize() )
+
+		local bitmap_dbid = db:last_insert_rowid()
+
+		local exec_add_ilbm = assert(db:prepare [[
+
+			INSERT INTO iff_ilbm (bitmap_dbid, x, y)
+			VALUES (:bitmap_dbid, :x, :y)
+
+		]])
+
+		assert( exec_add_ilbm:bind_int(':x', iff.x) )
+		assert( exec_add_ilbm:bind_int(':y', iff.y) )
+		assert( exec_add_ilbm:bind_int64(':bitmap_dbid', bitmap_dbid) )
+
+		assert( assert( exec_add_ilbm:step() ) == 'done' )
+		assert( exec_add_ilbm:finalize() )
+
 	end
 end
 
@@ -118,7 +172,9 @@ function reader_proto:iff_form(iff)
 			local chunk_reader = R.fromstring(chunk_data)
 			chunk_reader:inject 'bindata'
 			chunk_reader:inject(reader_proto)
-			chunk_handler(chunk_reader, iff)
+			if chunk_handler(chunk_reader, iff) == 'default' then
+				iff[#iff+1] = {type=chunk_type, data=chunk_data}
+			end
 		else
 			-- "BODY" (signed bytes -128 to 127)
 			-- "NAME"
@@ -127,6 +183,57 @@ function reader_proto:iff_form(iff)
 			-- "(c) "
 			iff[#iff+1] = {type=chunk_type, data=chunk_data}
 		end
+		if chunk_len % 2 == 1 then
+			data_reader:skip(1) -- padding
+		end
+	end
+end
+
+function reader_proto:iff_chunk_BODY(iff)
+	if iff.type == 'ILBM' then
+		local nextbyte
+		if iff.compression == 'packer' then
+			nextbyte = coroutine.wrap(function()
+				while true do
+					local code = self:int8()
+					if code >= 0 then
+						for i = 1, 1 + code do
+							coroutine.yield(self:uint8())
+						end
+					elseif code > -128 then
+						local rep = self:uint8()
+						for i = 1, 1 - code do
+							coroutine.yield(rep)
+						end
+					end
+				end
+			end)
+		elseif iff.compression == nil then
+			nextbyte = function()
+				return self:uint8()
+			end
+		else
+			error('unsupported compression type: ' .. iff.compression)
+		end
+		local buf = ffi.new('uint8_t[' .. (iff.width * iff.height) .. ']')
+		local rowbytes = bit.lshift(bit.rshift(iff.width + 15, 4), 1)
+		local w = iff.width
+		for scanline = 0, iff.height-1 do
+			for bitplane = 0, iff.bitplanes-1 do
+				for rowbyte = 0, rowbytes-1 do
+					local base_ptr = buf + (scanline * iff.width) + (rowbyte * 8)
+					local byte = nextbyte()
+					for i = 0, 7 do
+						if 0 ~= bit.band(byte, bit.rshift(0x80, i)) and (rowbyte*8 + i) < w then
+							base_ptr[i] = bit.bor(base_ptr[i], bit.lshift(1, bitplane))
+						end
+					end
+				end
+			end
+		end
+		iff.pixel_data = ffi.string(buf, iff.width * iff.height)
+	else
+		return 'default'
 	end
 end
 
@@ -158,6 +265,11 @@ function reader_proto:iff_chunk_BMHD(iff)
 	iff.bitplanes = self:uint8()
 	iff.masking = self:uint8()
 	iff.compression = self:uint8()
+	if iff.compression == 0 then
+		iff.compression = nil
+	elseif iff.compression == 1 then
+		iff.compression = 'packer'
+	end
 	iff.padding = self:uint8() -- always 0?
 	iff.transparency = self:uint16be()
 	iff.aspect_ratio_x = self:uint8()
@@ -169,7 +281,7 @@ end
 function reader_proto:iff_chunk_CMAP(iff)
 	local start = self:pos()
 	local stop = self:pos('end')
-	self:pos(start)
+	self:pos('set', start)
 	local count = math.floor((stop - start) / 3)
 	iff.palette = self:blob(count * 3)
 end
